4b1ea17315c6016e68e38cb405da2cf054f9cc8d
Erik Dzotsenidze <erikdzotsenidze@gmail.com>
2025-01-27 19:33:39 +0200

    feat: track page view on route change

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,30 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+import Vue from "vue";
+import Router from "vue-router";
 import Meta from "vue-meta"; // https://github.com/declandewet/vue-meta
 
 Vue.use(Router);
-Vue.use(Meta,{keyName: 'metaInfo'});
+Vue.use(Meta, { keyName: "metaInfo" });
 
-const FrontPage = () => import('../views/FrontPage.vue')
-const ItemPage = () => import('../views/ItemPage.vue')
-const StaticPage = () => import('../views/StaticPage.vue')
+const FrontPage = () => import("../views/FrontPage.vue");
+const ItemPage = () => import("../views/ItemPage.vue");
+const StaticPage = () => import("../views/StaticPage.vue");
 
-export default new Router({
-  mode: 'history',
+const router = new Router({
+  mode: "history",
   // fallback: false,
   // scrollBehavior: () => ({ y: 0 }),
   routes: [
-    {path: '/:id', name: 'ItemPage', component: ItemPage},
-    {path: '/', name: 'FrontPage', component: FrontPage},
-    {path: '/page/:id', component: StaticPage },
-    {path: '*', redirect: '/'}
+    { path: "/:id", name: "ItemPage", component: ItemPage },
+    { path: "/", name: "FrontPage", component: FrontPage },
+    { path: "/page/:id", component: StaticPage },
+    { path: "*", redirect: "/" }
   ]
-})
+});
+
+router.afterEach((to, from) => {
+  if (window._paq) {
+    window._paq.push(["trackPageView"]);
+  }
+});
+
+export default router;
